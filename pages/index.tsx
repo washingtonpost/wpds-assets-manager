b4d9afcb73a94ff0cab6c79ed8157004b6553a98
artmsilva <artmsilva@gmail.com>
2021-12-10 12:07:14 -0500

    docs: add assets directory

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,31 @@
+import { styled } from "@washingtonpost/ui-theme";
+import * as AllAssets from "./../asset/index";
+
+const AssetContainer = styled("article", {
+  border: "1px solid $subtle",
+  background: "$accessible",
+  padding: "$100",
+  borderRadius: "$100",
+});
+
 const Homepage = () => {
-  return <h1>WPDS Assets Manager</h1>;
+  return (
+    <>
+      <h1>WPDS Assets Manager</h1>
+
+      {Object.keys(AllAssets).map((Asset) => {
+        const Component = AllAssets[Asset];
+        return (
+          <section key={Asset}>
+            <AssetContainer>
+              <h2>{Asset}</h2>
+              <Component />
+            </AssetContainer>
+          </section>
+        );
+      })}
+    </>
+  );
 };
 
 export default Homepage;
