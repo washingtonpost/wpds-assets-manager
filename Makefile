274a51c56f6e3442aff25cf991127b8a2250e515
artmsilva <artmsilva@gmail.com>
2022-03-23 10:51:04 -0400

    try out sprite asset

--- Makefile
+++ Makefile
@@ -5,8 +5,12 @@ copy-assets-into-public-directory:
 svg-to-react:
 	npx @svgr/cli --out-dir build src
 
+sprite-gen:
+	node icon-gen.js
+
 build-library:
 	npx svgo src/*.svg
+	make sprite-gen
 	make svg-to-react
 	npx tsup build/*.tsx --minify --format esm,cjs --dts --sourcemap --legacy-output --outDir asset
 	make copy-assets-into-public-directory
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,34 @@
 import * as Kit from "@washingtonpost/wpds-ui-kit";
 import * as AllAssets from "./../asset/esm/index";
 
+export type IconName = string;
+
+export interface IconProps {
+  name: IconName;
+  className?: string;
+  fill?: string;
+}
+
+export function getIconUrl(name: IconName): string {
+  return `/icons-embed/${name}.svg`;
+}
+
+const StyledSVG = Kit.styled("svg", {
+  size: "$400",
+});
+
+export function SpriteAsset(props: IconProps) {
+  return (
+    <StyledSVG
+      css={{
+        color: props.fill,
+      }}
+    >
+      <use href={`${getIconUrl(props.name)}#wpds-asset-id`} />
+    </StyledSVG>
+  );
+}
+
 const Section = Kit.styled("section", {
   display: "grid",
   gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))",
@@ -18,6 +46,12 @@ const Homepage = () => {
   return (
     <Section>
       <h1>WPDS Assets Manager</h1>
+
+      <h2>
+        test using the new sprite asset component <pre>SpriteAsset</pre>
+      </h2>
+      <SpriteAsset name="15-back" fill="$cta" aria-hidden="true" />
+
       {Object.keys(AllAssets).map((Asset) => {
         const Component = AllAssets[Asset];
         return (
