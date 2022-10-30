fd9f92b71de01c6c88d6a2c70928c97e7ad1a652
Arturo Silva <arturo.silva@washpost.com>
2022-10-29 23:21:59 -0400

    fix: use local link

--- app/app.jsx
+++ app/app.jsx
@@ -1,5 +1,5 @@
 import * as Kit from "@washingtonpost/wpds-ui-kit";
-import * as AllAssets from "./../asset";
+import * as AllAssets from "@washingtonpost/wpds-assets";
 
 const Section = Kit.styled("section", {
   display: "grid",
