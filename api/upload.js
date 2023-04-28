6aa372148237408a9ce0bf93c0c675d142e4aced
Arturo Silva <arturo.silva@washpost.com>
2023-04-27 23:11:36 -0400

    safd

--- api/upload.js
+++ api/upload.js
@@ -13,57 +13,58 @@ const upload = async (req, res) => {
   // read the file to upload
   const fileContent = fs.readFileSync("/tmp/boop.svg");
 
-  octokit.repos
-    .createOrUpdateFile({
-      owner,
-      repo,
-      path: "boop.svg",
-      message: "Add new file",
-      content: fileContent.toString("base64"),
-    })
-    .then(() => {
-      console.log(`File ${fileContent} uploaded successfully`);
+  // octokit.repos
+  //   .createOrUpdateFile({
+  //     owner,
+  //     repo,
+  //     path: "boop.svg",
+  //     message: "Add new file",
+  //     content: fileContent.toString("base64"),
+  //   })
+  //   .then(() => {
+  //     console.log(`File ${fileContent} uploaded successfully`);
 
-      // create a new branch
-      const branchName = `new-file-${Date.now()}`;
-      const mainBranch = "main";
-      octokit.git
-        .createRef({
-          owner,
-          repo,
-          ref: `refs/heads/${branchName}`,
-          sha: mainBranch,
-        })
-        .then(() => {
-          console.log(`Branch ${branchName} created successfully`);
+  //     // create a new branch
+  //     const branchName = `new-file-${Date.now()}`;
+  //     const mainBranch = "main";
+  //     octokit.git
+  //       .createRef({
+  //         owner,
+  //         repo,
+  //         ref: `refs/heads/${branchName}`,
+  //         sha: mainBranch,
+  //       })
+  //       .then(() => {
+  //         console.log(`Branch ${branchName} created successfully`);
 
-          // create a new pull request
-          octokit.pulls
-            .create({
-              owner,
-              repo,
-              title: `Add ${fileContent}`,
-              head: branchName,
-              base: mainBranch,
-              body: "Please review and merge this file",
-            })
-            .then(() => {
-              console.log(`Pull request created successfully`);
-            })
-            .catch((error) => {
-              console.error(`Error creating pull request: ${error}`);
-            });
-        })
-        .catch((error) => {
-          console.error(`Error creating branch: ${error}`);
-        });
-    })
-    .catch((error) => {
-      console.error(`Error uploading file: ${error}`);
-    });
+  //         // create a new pull request
+  //         octokit.pulls
+  //           .create({
+  //             owner,
+  //             repo,
+  //             title: `Add ${fileContent}`,
+  //             head: branchName,
+  //             base: mainBranch,
+  //             body: "Please review and merge this file",
+  //           })
+  //           .then(() => {
+  //             console.log(`Pull request created successfully`);
+  //           })
+  //           .catch((error) => {
+  //             console.error(`Error creating pull request: ${error}`);
+  //           });
+  //       })
+  //       .catch((error) => {
+  //         console.error(`Error creating branch: ${error}`);
+  //       });
+  //   })
+  //   .catch((error) => {
+  //     console.error(`Error uploading file: ${error}`);
+  //   });
 
   // send a success response when the file is uploaded
   req.on("end", () => {
+    console.log(fileContent)
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ message: "File uploaded successfully" }));
   });
